db7fa8052b06c5b4ddd20141eaada2f14c60690b
somechandra <sen.somechandra@gmail.com>
2025-04-05 18:28:38 +0530

    var added and modified

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBSA2u3VhdKrLIQE4qzkHHRHnMmbgTvfIc",
-  authDomain: "interviewpro-2db10.firebaseapp.com",
-  projectId: "interviewpro-2db10",
-  storageBucket: "interviewpro-2db10.firebasestorage.app",
-  messagingSenderId: "739618142543",
-  appId: "1:739618142543:web:1023470963d6fa452eed3a",
-  measurementId: "G-LCEVH89RDD",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
